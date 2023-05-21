294b101186af4918a007939612d4fdd912d8fefd
Emanalabsi <emanabsi30@gmail.com>
2023-05-21 22:04:19 +0300

    Handle form States using formik & validate inputs

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Signup from "./components/auth/signup";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Signup />
+  </React.StrictMode>
+);
