97a813fad06ff861d503b45312d832463d8faafe
AbdallahAbujazar <abdallah.abujazar@iths.se>
2023-05-21 16:02:57 +0300

    Routes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,53 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import Root from './componets/root.jsx';
+import HomeComponets from './componets/homecomponets';
+import ProductDetails from './componets/productdetails.jsx';
+import Cart from './componets/cart.jsx';
+//import Login from './componets/auth/login.jsx';
+//import Signup from './componets/auth/signup.jsx';
+
 import './app.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "/",
+        element: <HomeComponets />,
+      },
+      {
+        path: "/product/:id",
+        element: <ProductDetails />,
+        
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/login",
+        element: <Cart />,
+      },
+      {
+        path: "/signup",
+        element: <Cart />,
+      },
+    ],
+  },
+ 
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+      </React.StrictMode>,
 )
