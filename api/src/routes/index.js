e76e5aef53ed4b880c25a3b54e576c359757bf46
Emanalabsi <emanabsi30@gmail.com>
2023-05-23 16:44:26 +0300

    resolve conflict

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,17 +1,14 @@
 const express = require('express');
 
 const router = express.Router();
+const categoryRouter = require('./category');
 const productRouter = require('./product');
-<<<<<<< HEAD
 const userRouter = require('./auth');
 
 router.use('/api/v1/', productRouter);
 router.use('/api/v1/', userRouter);
-=======
-const categoryRouter = require('./category');
 
 router.use('/api/v1/', productRouter);
 router.use('/api/v1/', categoryRouter);
->>>>>>> e29470ba736c68d21c343b25d5f4d150ba89dd3a
 
 module.exports = router;
